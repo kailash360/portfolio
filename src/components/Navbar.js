b90ac9416e10073922c064003b4103dedc2ca495
kailash360 <kailashkejriwal21@gmail.com>
2021-07-17 16:06:59 +0530

    Created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../static/Navbar.css'
+
+function Navbar() {
+    
+    return (
+        <nav id="nav" >
+            <ul>
+                <li><i className="fas fa-user"></i>&nbsp;&nbsp; <a href="#" className="sections">Profile</a></li>
+                <li><i className="fas fa-info-circle"></i>&nbsp;&nbsp; <a href="#" className="sections">About Me</a></li>
+                <li><i className="fas fa-tools"></i>&nbsp;&nbsp; <a href="#" className="sections">Skills</a></li>
+                <li><i className="fas fa-tasks"></i>&nbsp;&nbsp; <a href="#" className="sections">Projects</a></li>
+                <li><i className="fas fa-envelope"></i>&nbsp;&nbsp; <a href="#" className="sections">Contact Me</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
