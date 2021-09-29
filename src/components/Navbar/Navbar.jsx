c9ae4b2a8466229be6b53c6743a7cf989fabfc25
kailash360 <kailashkejriwal21@gmail.com>
2021-09-30 04:03:21 +0530

    added profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,11 +11,11 @@ function Navbar() {
                 </p>
             </span>
             <ul>
-                <li><i className="fas fa-user"></i>&nbsp;&nbsp; <a href="#" className="sections">Profile</a></li>
-                <li><i className="fas fa-info-circle"></i>&nbsp;&nbsp; <a href="#" className="sections">About Me</a></li>
-                <li><i className="fas fa-tools"></i>&nbsp;&nbsp; <a href="#" className="sections">Skills</a></li>
-                <li><i className="fas fa-tasks"></i>&nbsp;&nbsp; <a href="#" className="sections">Projects</a></li>
-                <li><i className="fas fa-envelope"></i>&nbsp;&nbsp; <a href="#" className="sections">Contact Me</a></li>
+                <li><a href="#" className="sections">Profile</a></li>
+                <li><a href="#" className="sections">About Me</a></li>
+                <li><a href="#" className="sections">Skills</a></li>
+                <li><a href="#" className="sections">Projects</a></li>
+                <li><a href="#" className="sections">Contact Me</a></li>
             </ul>
         </nav>
     )
