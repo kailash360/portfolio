d9b9fdc3774e14d5d7e73350389e93eb8eaeb509
kailash360 <kailashkejriwal21@gmail.com>
2021-10-17 23:13:43 +0530

    added experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './static/App.css';
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile';
 import Skills from './components/Skills/Skills'
+import Experience from './components/Experience/Experience'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Profile></Profile>
       <Skills></Skills>
+      <Experience></Experience>
     </>
   );
 }
