e5bfa1a7d6c97e787ad33d70da71677762436891
kailash360 <kailashkejriwal21@gmail.com>
2021-11-17 21:51:41 +0530

    minor fix

--- components/Experience/Card/Card.jsx
+++ components/Experience/Card/Card.jsx
@@ -7,7 +7,7 @@ function Card({key,index,role,company,from,to,color}) {
             <p className={styles.card_number} style={{color:color}} >{index<10?("0"+index):index}</p>
             <h2 className={styles.role}> <i className="far fa-user fa-sm"></i> {role}</h2>
             <h3 className={styles.company}><i className="far fa-building fa-md"></i>  {company}</h3>
-            <p className={styles.duration}><i class="far fa-calendar fa-md"></i> {from + "-" + to}</p>
+            <p className={styles.duration}><i className="far fa-calendar fa-md"></i> {from + "-" + to}</p>
         </div>
     )
 }
