499acb6f76893134f6c6b263a8bc8623707d30f4
kailash360 <kailashkejriwal21@gmail.com>
2022-01-18 17:18:20 +0530

    updated data

--- data/Experience.js
+++ data/Experience.js
@@ -2,7 +2,7 @@ export const ExperienceList = [{
     company: "InfuzeX Ventures",
     role: "Full Stack Developer",
     from: "August 2021",
-    to: "Present",
+    to: "November 2021",
     color: "linear-gradient(-20deg, #b721ff 0%, #21d4fd 100%)",
     logo: 'https://infuzex.in/images/logo/infuzex-logo-tp-2.png'
 }]
