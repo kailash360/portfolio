4f755957cb9420c9505ca020ef899921fbf88c24
kailash360 <kailashkejriwal21@gmail.com>
2022-05-18 01:34:39 +0530

    Added projects

--- data/Projects.js
+++ data/Projects.js
@@ -1,4 +1,20 @@
 export const ProjectsList = [{
+        name: 'DeTrace',
+        description: 'A decentralized supply-chain management system that tracks down the product distribution from manufacturer-level to consumer-level using the blockchain events',
+        image: '/Projects/detrace.jpeg',
+        github: 'https://github.com/kailash360/DeTrace',
+        hosted: false,
+        host_link: 'https://github.com/kailash360/DeTrace'
+    },
+    {
+        name: 'DeQuora',
+        description: 'A decentralized discussion forum that prevents the manipulation of the topics and content by any third party organization',
+        image: '/Projects/dequora.png',
+        github: 'https://github.com/kailash360/De-Quora',
+        hosted: false,
+        host_link: 'https://github.com/kailash360/De-Quora'
+    },
+    {
         name: 'Snap Stock',
         description: 'A solidity-smart-contract based DApp that lets the users upload their images in exchange of gas fee and tip the images of others, based on their choice',
         image: '/Projects/Snapstock.png',
