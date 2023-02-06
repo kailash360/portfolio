ff4c9e2b15694617ef9b9dd40f84306079d072fc
Kailash Kejriwal <kailashkejriwal21@gmail.com>
2023-02-07 01:54:06 +0530

    updated resume

--- components/Profile/ProfileText/ProfileText.jsx
+++ components/Profile/ProfileText/ProfileText.jsx
@@ -9,7 +9,7 @@ function ProfileText() {
             <h2 style={{marginBlockEnd:"5rem"}} >I’m Kailash, <br/>
                 A <Role/> <br/>
             </h2>
-            <a href = "https://drive.google.com/file/d/11PyE395kU7H0iiFblW0zazUWEZS9L7nS/view?usp=sharing" target="_blank" without rel="noreferrer" className={styles.a} >Download CV &nbsp; <i className="fas fa-cloud-download-alt fa-md"></i></a>
+            <a href = "https://drive.google.com/file/d/1gA7toMJTcOE82Vl66FxivbQK4_bnaQeM/view?usp=sharing" target="_blank" without rel="noreferrer" className={styles.a} >Download CV &nbsp; <i className="fas fa-cloud-download-alt fa-md"></i></a>
         </div>
     )
 }
